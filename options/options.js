0776ab1303ba24b7b1b855836e5b1e1684dc2bca
Crowdbotics <engineering@crowdbotics.com>
2023-05-08 00:46:05 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "aged-feather-dev-81926",
+  url: "https://aged-feather-dev-81926.botics.co",
+  api: "https://aged-feather-dev-81926.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
